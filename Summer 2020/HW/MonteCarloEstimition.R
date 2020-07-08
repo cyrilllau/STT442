c647fb39e53fc7c22df9d25acc35503fea1647f7
cyrilllau <cyrilllau@gmail.com>
2020-07-08 18:48:18 -0400

    Add Code

---
+++ Summer 2020/HW/MonteCarloEstimition.R
@@ -0,0 +1,12 @@
+x = runif(1000,-2,2)
+p2 = function(x){exp(-x^2/2)}
+mean(p2(x))
+4*mean(p2(x)) #Approximation
+#4*sd(p2(x))/sqrt(1000) #Estimate of the approximation
+approx = c()
+for (i in 1:100){
+  x = runif(1000,-2,2)
+  approx = c(approx,4*mean(p2(x)))
+}
+sd(approx)
+
